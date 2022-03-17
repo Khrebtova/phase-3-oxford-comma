116d2a2c50ac81b72f33dd149d51bfc2b1ff9f7f
Kseniia McFadden <mcfaddenkseniia@gmail.com>
2022-03-17 10:40:27 -0700

    commit tree dd6234c43efe2d9658b3e861cf023257c9290d73

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
-def oxford_comma(array)
+require 'pry'
 
+def oxford_comma(array)
+    if array.length == 2
+        array.join(" and ")
+    elsif array.length < 2
+        array.join     
+    elsif array.length > 2
+        x= array.length-1
+        array[x]= "and #{array[x]}"
+        array.join(", ")
+    end
 end
+
+
